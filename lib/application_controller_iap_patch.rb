eed316a7ab54776c1d9d6e5e42cb1d4c166b7a7b
Samuel Melrose <sam.melrose@a1comms.com>
2019-07-11 14:21:49 +0000

    try auto user creation

--- lib/application_controller_iap_patch.rb
+++ lib/application_controller_iap_patch.rb
@@ -8,14 +8,32 @@ module GoogleIAP
 
             if (emailaddress = request.headers["X-Goog-Authenticated-User-Email"].to_s.presence)
                 emailaddress.sub! 'accounts.google.com:', ''
-                
+
                 su = User.find_by_mail(emailaddress)
                 if su && su.active?
                     logger.info("  IAP Login for : #{emailaddress} (id=#{su.id})") if logger
                     user = su
                 else
-                    logger.error("  Failed IAP Login for : #{emailaddress} (not found)") if logger
-                    render_error :message => 'Automatic Login User Not Found', :status => 403
+                    attrs = {
+                        :login => emailaddress,
+                        :firstname => emailaddress.split('@')[0],
+                        :lastname => emailaddress.split('@')[1],
+                        :mail => emailaddress,
+                        :language => Setting.default_language
+                    }
+
+                    user = new User(attrs)
+
+                    user.activate
+
+                    if user.save
+                        user.reload
+                        logger.info("User '#{user.login}' created from IAP login: #{emailaddress} (id=#{su.id})") if logger
+                    else
+                        user = nil
+                        logger.error("  Failed IAP user creation for : #{emailaddress}") if logger
+                        render_error :message => 'IAP Automatic User Creation Failed', :status => 403
+                    end
                 end
             end
 
